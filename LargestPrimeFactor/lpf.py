c5f335923a6bc33d5032e5ea2149c6aa4bcbc654
Christopher <beiercm@gmail.com>
2015-05-14 20:57:59 -0400

    changed lpf.py

--- LargestPrimeFactor/lpf.py
+++ LargestPrimeFactor/lpf.py
@@ -1,13 +1,17 @@
 def findLargestPrimeFactor(n):
 	div = 2
 
+	#gets the even factors out of the way
+	while n % div == 0:
+		n /= div
+
+	div = 3
+
 	while n > div:
 		if n % div == 0:
 			n /= div
 		else:
-			div += 1
-
-		print(n)
+			div += 2
 
 	return div
 
